9a71122390224d25072ab077166ffc3072273366
Lo Dev <dev@divethree.com>
2022-01-04 12:04:34 -0800

    Add new item scroll and feature to remove it from scene on player contact.

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -1,21 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using Data;
 using UnityEngine;
 
 public class CollectibleItem : MonoBehaviour
 {
     public ItemData item;
+    public BoxCollider2D collider2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        collider2d = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.gameObject.CompareTag("Player"))
+        {
+            // TODO: add item to players inventory
+            Destroy(gameObject);
+        }
     }
 }

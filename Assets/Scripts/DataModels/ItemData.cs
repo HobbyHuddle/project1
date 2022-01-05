9bc8316326eea4378541c64947a9cd2ddf614b97
diveonebot <dev@divethree.com>
2022-01-04 20:36:22 -0800

    Feature: Player item collection (#8)
    * Add new collectible item object.
    
    * Add new item scroll and feature to remove it from scene on player contact.
    
    * Add inventory feature for player item collection
    
    - renamed data folder for clarity
    - add data model: ItemData
    - add game object: PlayerCharacter
    
    * Apply prefab script for player character.

---
+++ Assets/Scripts/DataModels/ItemData.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace DataModels
+{
+    /// <summary>
+    /// Used to create and define new game items.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Item", menuName = "Game/Item")]
+    public class ItemData : ScriptableObject
+    {
+        public new string name;
+        public string description;
+        public Sprite icon;
+        public Color color;
+    }
+}

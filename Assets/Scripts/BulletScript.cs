d563ceb5e4dc38a74d48338fca1b38a404f33583
Injuryble98 <97007877+Injuryble98@users.noreply.github.com>
2022-01-05 06:44:32 +0100

    Improvement/shooting (#9)
    * Basic PowerUpSystem
    
    WIP
    
    -powerup system in place
    -changable cooldown for shots
    
    * Update Prototyping.unity
    
    * Added Advanced PowerUp System
    
    * Added PowerUps
    
    - customizable bounce powerup
    - customziable charge powerup
    
    * Value Tweaking
    
    * Added Dynamic Painting
    
    * 2 Changes
    
    - Paint splash scales with scale of bullet
    - change color of bullet
    
    Co-authored-by: diveonebot <dev@divethree.com>

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,10 +4,56 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public PowerUpSystem powerUpSystem;
+    public ShooterScript shooterScript;
+
+    public int bounceCount;
+
+    private void Awake()
+    {
+        powerUpSystem = GameObject.FindGameObjectWithTag("Player").transform.Find("PowerUpSystem").GetComponent<PowerUpSystem>();
+        shooterScript = GameObject.FindGameObjectWithTag("Player").transform.Find("Shooter").GetComponent<ShooterScript>();
+    }
+
+    private void Start()
+    {
+        bounceCount = powerUpSystem.bounce.BounceCount;
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.collider.transform.gameObject);
+        ContactPoint2D contact = collision.contacts[0];
+        Quaternion rot = Random.rotation;
+        rot.x = 0;
+        rot.y = 0;
+        Vector2 pos = contact.point;
 
-        GameObject.Destroy(this.transform.gameObject);
+        GameObject paint = Instantiate(shooterScript.environmentPaintPrefab , pos, rot, shooterScript.environmentPaintParent.transform);
+
+        paint.transform.localScale = transform.gameObject.transform.localScale;
+
+        paint.GetComponent<EnvironmentPaintScript>().paintColor = shooterScript.paintColor;
+
+       if (powerUpSystem.bounce.Active == true)
+        {
+            if (bounceCount == 0)
+            {
+                GameObject.Destroy(this.transform.gameObject);
+            }
+            else
+            {
+               bounceCount -= 1;
+            }
+        }
+        else
+        {
+            GameObject.Destroy(this.transform.gameObject);
+        }
     }
 }
--- Assets/Scripts/ShooterScript.cs
+++ Assets/Scripts/ShooterScript.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class ShooterScript : MonoBehaviour
 {
-    [SerializeField] private Vector2 dirToMouse, raycast2DEndPoint, raycast2DHitPoint;
-    [SerializeField] private GameObject raycast2DHitObject, bulletPrefab, bulletSpawnPoint, gun, player;
-    [SerializeField] private int bulletCollideLayer;
-    [SerializeField] private float projectileSpeed;
-    public float raycastDistance = 5f;
+    PowerUpSystem powerUpSystem;
 
+    private Vector2 dirToMouse, raycast2DEndPoint, raycast2DHitPoint, bubbleSize;
+    [SerializeField] private GameObject raycast2DHitObject, bulletPrefab, bulletsParent, bulletSpawnPoint, gun;
+    public GameObject environmentPaintPrefab, environmentPaintParent;
+    private int bulletCollideLayer;
+    [SerializeField] private float projectileSpeed, cooldown;
+    private float raycastDistance = 25;
+    [SerializeField] private bool cooldownRunning, chargeUpRunning, mouseButtonDown;
+    public Color paintColor;
+
+    private void Awake()
+    {
+        powerUpSystem = GameObject.FindGameObjectWithTag("Player").transform.Find("PowerUpSystem").GetComponent<PowerUpSystem>();
+    }
 
     private void Start()
     {
         bulletCollideLayer = LayerMask.GetMask("Platform");
+
+        bubbleSize = new Vector2(0.3f, 0.3f);
     }
 
     private void Update()
@@ -24,34 +35,116 @@ public class ShooterScript : MonoBehaviour
         float angle = Mathf.Atan2(dirToMouse.y, dirToMouse.x) * Mathf.Rad2Deg;
         gun.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
+
         //Left-Click To Shoot
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            mouseButtonDown = true;
+        }
+        else
+        {
+            mouseButtonDown = false;
+        }
+        if(mouseButtonDown)
+        {
+            if (cooldownRunning == false)
+            {
+                PowerUpOnShot();
+            }
+        }
+    }
+
+
+    private void PowerUpOnShot()
+    {
+        if(powerUpSystem.charge.Active == false)
         {
             Shoot();
+        }
+        else
+        {
+            if(chargeUpRunning == false)
+            {
+                StartCoroutine(ChargeUp());
+            }
+
+            if(mouseButtonDown == false)
+            {
+                StopCoroutine(ChargeUp());
+                Shoot();
+                bubbleSize = new Vector2(0.3f, 0.3f);
+                chargeUpRunning = false;
+            }
         }
     }
 
     private void Shoot()
     {
+        StartCoroutine(Cooldown());
+
         RaycastHit2D hit = Physics2D.Raycast(gun.transform.position, gun.transform.right, raycastDistance, bulletCollideLayer);
         raycast2DEndPoint = new Vector2(gun.transform.position.x, gun.transform.position.y) + (dirToMouse.normalized * raycastDistance);
-        
+
         if (hit.collider != null)
         {
-            Debug.Log("Did Hit " + raycast2DHitObject);
             Debug.DrawLine(bulletSpawnPoint.transform.position, hit.point, Color.green, 2);
             raycast2DHitObject = hit.collider.transform.gameObject;
             raycast2DHitPoint = hit.point;
         }
         else
         {
-            Debug.Log("Did NOT Hit!");
+            Debug.Log("Raycast did not hit any object! is there a gap in the level borders?");
             Debug.DrawLine(bulletSpawnPoint.transform.position, raycast2DEndPoint, Color.red, 2);
         }
 
 
-        GameObject clone = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation);
+        GameObject clone = Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, bulletSpawnPoint.transform.rotation, bulletsParent.transform);
+
+        clone.GetComponent<SpriteRenderer>().color = paintColor;
+
         var rb2D = clone.gameObject.GetComponent<Rigidbody2D>();
         rb2D.AddForce(clone.transform.right * projectileSpeed);
+
+        clone.transform.localScale = bubbleSize;
     }
+
+    IEnumerator Cooldown()
+    {
+        cooldownRunning = true;
+        yield return new WaitForSeconds(cooldown);
+        cooldownRunning = false;
+        yield break;
+    }
+
+    IEnumerator ChargeUp()
+    {
+        chargeUpRunning = true;
+
+        while(true)
+
+            if(mouseButtonDown == true)
+            {
+                if(powerUpSystem.charge.MaxChargeSize.x > bubbleSize.x)
+                {
+                    bubbleSize.x += powerUpSystem.charge.ChargeRatePerSecond * 0.1f;
+                    bubbleSize.y += powerUpSystem.charge.ChargeRatePerSecond * 0.1f;
+                    yield return new WaitForSeconds(0.1f);
+                }
+
+                else
+                {
+                    Debug.Log("Max chargeSize reached");
+                    yield return new WaitForSeconds(1f);
+                }
+            }
+            else
+            {
+                Shoot();
+                bubbleSize = new Vector2(0.3f, 0.3f);
+                chargeUpRunning = false;
+                yield break;
+            }
+    }
+
+
 }

1ebf34dc7c4b3cce9e327998168900ac08cae505
Lo Dev <dev@divethree.com>
2022-01-09 19:31:03 -0800

    Add level color palette creation tool.
    It should help us define the color scheme of each level in a way that allows us to programmatically change the colors during gameplay.

---
+++ Assets/Scripts/Shared/ColorScheme.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Shared
+{
+    [CreateAssetMenu(fileName = "New Color Scheme", menuName = "Game/Color Scheme")]
+    public class ColorScheme : ScriptableObject
+    {
+        [Header("Major Colors")]
+        public Color color1;
+        public Color color2;
+        public Color color3;
+        public Color color4;
+        public Color color5;
+
+        [Header("Minor Colors")] 
+        public Color minorColor1;
+        public Color minorColor2;
+        public Color minorColor3;
+    }
+}

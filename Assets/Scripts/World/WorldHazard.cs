eea91cbe45fb96ed3836b3f464c37abb3cded4df
Lo Dev <dev@divethree.com>
2022-01-24 20:10:16 -0800

    Create new object: world hazards (spikes)

---
+++ Assets/Scripts/World/WorldHazard.cs
@@ -0,0 +1,23 @@
+using System;
+using Characters;
+using UnityEngine;
+
+namespace World
+{
+    public enum DamageType { Spikes }
+    
+    public class WorldHazard : MonoBehaviour
+    {
+        public DamageType damageType;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Player"))
+            {
+                var player = col.GetComponent<CharacterController2D>();
+                Debug.Log("Player has died by " + damageType);
+                // player.Die();
+            }
+        }
+    }
+}

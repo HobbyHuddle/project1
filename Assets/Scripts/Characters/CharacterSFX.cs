584d489abe916ecc52662046826dead147aa4cf0
Lo Dev <dev@divethree.com>
2022-01-26 18:21:28 -0800

    Feature: Sound effects
    - Adds: shoot, run, jump and death
    - Minor fixes to level doors
    - Update level order in build settings

---
+++ Assets/Scripts/Characters/CharacterSFX.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Characters
+{
+    [CreateAssetMenu(fileName = "New Character SFX", menuName = "Game/Character SFX")]
+    public class CharacterSFX : ScriptableObject
+    {
+        public AudioClip runSfx;
+        public AudioClip deathSfx;
+        public AudioClip jumpSfx;
+    }
+}
---
+++ Assets/Scripts/Characters/RunBehavior.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+namespace Characters
+{
+    public class RunBehavior : StateMachineBehaviour
+    {
+
+        private PlayerCharacter player;
+        
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            player = animator.gameObject.GetComponentInChildren<PlayerCharacter>();
+            player.PlayerRunAudio();
+        }
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            player.audioSource.Stop();
+        }
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
+    }
+}
